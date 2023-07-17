d40c11f95ecba8231a0594acf4cf6b8f091d6d28
Stéphane Manciot <stephane.manciot@gmail.com>
2023-07-17 15:50:18 +0200

    upgrade server dependencies + add swagger path prefix

--- project/src/main/scala/app/softnetwork/sbt/build/Versions.scala
+++ project/src/main/scala/app/softnetwork/sbt/build/Versions.scala
@@ -2,9 +2,9 @@ package app.softnetwork.sbt.build
 
 object Versions {
 
-  val genericPersistence = "0.3.2.3"
+  val genericPersistence = "0.3.2.4"
 
-  val scheduler = "0.3.2.1"
+  val scheduler = "0.3.2.2"
 
   val scalatest = "3.1.1"
 }
