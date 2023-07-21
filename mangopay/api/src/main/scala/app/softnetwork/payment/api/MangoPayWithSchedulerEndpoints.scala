ea9fd7f4aac7b18f0a65e506690ec922a22fd420
Stéphane Manciot <stephane.manciot@gmail.com>
2023-07-21 21:05:21 +0200

    to fix compilation error

--- mangopay/api/src/main/scala/app/softnetwork/payment/api/MangoPayWithSchedulerEndpoints.scala
+++ mangopay/api/src/main/scala/app/softnetwork/payment/api/MangoPayWithSchedulerEndpoints.scala
@@ -1,19 +1,14 @@
 package app.softnetwork.payment.api
 
 import akka.actor.typed.ActorSystem
+import app.softnetwork.api.server.ApiEndpoint
 import app.softnetwork.persistence.schema.SchemaProvider
 import app.softnetwork.scheduler.launch.SchedulerEndpoints
 import com.softwaremill.session.CsrfCheck
-import sttp.capabilities
-import sttp.capabilities.akka.AkkaStreams
-import sttp.tapir.server.ServerEndpoint
-
-import scala.concurrent.Future
 
 trait MangoPayWithSchedulerEndpoints extends SchedulerEndpoints with MangoPayEndpoints {
   _: SchemaProvider with CsrfCheck =>
 
-  override def endpoints
-    : ActorSystem[_] => List[ServerEndpoint[AkkaStreams with capabilities.WebSockets, Future]] =
-    system => super.endpoints(system) ++ schedulerEndpoints(system).endpoints
+  override def endpoints: ActorSystem[_] => List[ApiEndpoint] =
+    system => super.endpoints(system) :+ schedulerEndpoints(system)
 }
