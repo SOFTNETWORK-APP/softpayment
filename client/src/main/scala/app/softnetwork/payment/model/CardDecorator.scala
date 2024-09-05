f447974af3c0325ce834aeca1c556eb44d8c425b
Stéphane Manciot <stephane.manciot@gmail.com>
2024-09-05 17:08:59 +0200

    to fix parsing expiration date + expired will return true if any parsing error is raised

--- client/src/main/scala/app/softnetwork/payment/model/CardDecorator.scala
+++ client/src/main/scala/app/softnetwork/payment/model/CardDecorator.scala
@@ -1,19 +1,32 @@
 package app.softnetwork.payment.model
 
 import app.softnetwork.payment.model
+import com.typesafe.scalalogging.StrictLogging
 
 import java.text.SimpleDateFormat
 import java.util.Date
 import scala.util.{Failure, Success, Try}
 
-trait CardDecorator { self: Card =>
+trait CardDecorator extends StrictLogging { self: Card =>
   lazy val expired: Boolean = {
-    val sdf = new SimpleDateFormat("MMyy")
+    val pattern =
+      expirationDate.split("/").toSeq match {
+        case Seq(_, y) =>
+          if (y.length == 2) {
+            "MM/yy"
+          } else {
+            "MM/yyyy"
+          }
+        case _ =>
+          "MMyy"
+      }
+    val sdf = new SimpleDateFormat(pattern)
     Try(sdf.parse(expirationDate)) match {
       case Success(date) =>
         sdf.parse(sdf.format(new Date())).after(date)
-      case Failure(_) =>
-        false
+      case Failure(f) =>
+        logger.error(s"Error parsing expiration date $expirationDate: ${f.getMessage}")
+        true
     }
   }
 
