cb32e9ce2dc742ae0db60b6996a1ea717738adef
Stéphane Manciot <stephane.manciot@gmail.com>
2024-09-05 17:07:53 +0200

    fix json formats for logging purpose

--- common/src/main/scala/app/softnetwork/payment/spi/PaymentContext.scala
+++ common/src/main/scala/app/softnetwork/payment/spi/PaymentContext.scala
@@ -2,7 +2,9 @@ package app.softnetwork.payment.spi
 
 import app.softnetwork.payment.config.ProviderConfig
 import app.softnetwork.payment.model.SoftPayAccount
+import app.softnetwork.payment.serialization.paymentFormats
 import com.typesafe.scalalogging.Logger
+import org.json4s.Formats
 
 trait PaymentContext {
 
@@ -11,4 +13,6 @@ trait PaymentContext {
   implicit def provider: SoftPayAccount.Client.Provider
 
   implicit def config: ProviderConfig
+
+  implicit def formats: Formats = paymentFormats
 }
